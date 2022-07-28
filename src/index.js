f6bc2a578fd0d1aa0b6bebe7be32aa80e10e9c71
anmaiden <aannna123@gmail.com>
2022-07-28 17:18:03 +0300

    add task solutuion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let lengthStr = str.length;
+  let leftBrackets, rightBrackets;
+  for ([leftBrackets, rightBrackets] of bracketsConfig) {
+    str = str.split(`${leftBrackets}${rightBrackets}`).join('');
+  }
+  if (str.length === 0) return true;
+  else if (str.length === lengthStr) return false;
+  return check(str, bracketsConfig);
 }
